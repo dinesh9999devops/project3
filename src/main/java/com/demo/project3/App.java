22e419be96db24e8fef38ad7adc302070d16a747
dinesh9999devops <dineshtopula9999@gmail.com>
2019-04-04 06:55:19 +0000

    p3

---
+++ src/main/java/com/demo/project3/App.java
@@ -0,0 +1,13 @@
+package com.demo.project3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
